2d8ab57ee75160a1fd436598214d86a61c25f182
Arnold Supriyadi <arnoldsupriyadi@gmail.com>
2023-02-01 19:55:30 +0700

    add new plugin

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    stagePadding: 50,
+    loop: true,
+    margin: 10,
+    rtl: true,
+    lazyLoad: true,
+    autoHeight: true,
+    pullDrag: true,
+    dots: false,
+  });
+});
