705c3643424e952022c8b2069daa417b77af6eee
Arnold Supriyadi <arnoldsupriyadi@gmail.com>
2023-02-06 13:59:34 +0700

    relayout model

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
-    stagePadding: 50,
-    loop: true,
     margin: 10,
-    rtl: true,
-    lazyLoad: true,
-    autoHeight: true,
-    pullDrag: true,
+    loop: true,
+    autoWidth: true,
+    items: 4,
     dots: false,
+    nav: true,
   });
 });
